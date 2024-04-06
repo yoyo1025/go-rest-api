0889679dba88709966c9e494cebc7d02f59ff07b
yoyo <hsjejxys@gmail.com>
2024-04-07 01:03:49 +0900

    Liginメソッドの追加

--- usecase/user_usecase.go
+++ usecase/user_usecase.go
@@ -3,7 +3,10 @@ package usecase
 import (
 	"go-rest-api/model"
 	"go-rest-api/repository"
+	"os"
+	"time"
 
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -21,9 +24,6 @@ func NewUserUsecase(ur repository.IUserRepository) IUserUsecase {
 }
 
 func (uu *userUsecase) SignUp(user model.User) (model.UserResponse, error) {
-	if err := uu.uv.UserValidate(user); err != nil {
-		return model.UserResponse{}, err
-	}
 	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
 	if err != nil {
 		return model.UserResponse{}, err
@@ -37,4 +37,25 @@ func (uu *userUsecase) SignUp(user model.User) (model.UserResponse, error) {
 		Email: newUser.Email,
 	}
 	return resUser, nil
+}
+
+
+func (uu *userUsecase) Login(user model.User) (string, error) {
+	storedUser := model.User{}
+	if err := uu.ur.GetUserByEmail(&storedUser, user.Email); err != nil {
+		return "", err
+	}
+	err := bcrypt.CompareHashAndPassword([]byte(storedUser.Password), []byte(user.Password))
+	if err != nil {
+		return "", err
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": storedUser.ID,
+		"exp":     time.Now().Add(time.Hour * 12).Unix(),
+	})
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
 }
