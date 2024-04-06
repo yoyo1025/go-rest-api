30acd9bb8b67fdaf9582d42892937a232f9f618d
yoyo <hsjejxys@gmail.com>
2024-04-07 01:08:18 +0900

    repositoryからDI

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -1,8 +1,20 @@
 package repository
 
-import "go-rest-api/model"
+import (
+	"go-rest-api/model"
+
+	"gorm.io/gorm"
+)
 
 type IUserRepository interface {
 	GetUserByEmail(user *model.User, email string) error
 	CreateUser(user *model.User) error
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) IUserRepository{
+	return &userRepository{db}
 }
