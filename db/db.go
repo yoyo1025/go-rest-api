3b54e982e984a9d85311286f2ea6510bdaffdfdd
yoyo <hsjejxys@gmail.com>
2024-04-06 23:36:14 +0900

    dbを起動する関数を定義

---
+++ db/db.go
@@ -0,0 +1,18 @@
+package db
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+)
+
+func NewDB() *gorm.DB {
+	if os.Getenv("GO_ENV") == "dev" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+}
