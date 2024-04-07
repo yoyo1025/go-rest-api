af456cff18664452f8095b6d25e12394453651d5
yoyo <hsjejxys@gmail.com>
2024-04-07 14:20:17 +0900

    完成版

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -15,6 +15,10 @@ type userRepository struct {
 	db *gorm.DB
 }
 
+func NewUserRepository(db *gorm.DB) IUserRepository {
+	return &userRepository{db}
+}
+
 func (ur *userRepository) GetUserByEmail(user *model.User, email string) error {
 	if err := ur.db.Where("email=?", email).First(user).Error; err != nil {
 		return err
